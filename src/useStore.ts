93336aaf00125119599496faa0b736a6656f4058
HYEOK999 <iasg2004@naver.com>
2020-04-15 23:00:20 +0900

    feat : Updated useStore

--- src/useStore.ts
+++ src/useStore.ts
@@ -1,8 +1,28 @@
-import { counter } from './stores/counter';
-import { market } from './stores/market';
+// import { counter } from './stores/counter';
+// import { market } from './stores/market';
 
-const useStore = () => {
-  return { counter, market };
+// const useStore = () => {
+//   return { counter, market };
+// };
+
+// export default useStore;
+import React from 'react';
+import { useObserver } from 'mobx-react-lite';
+
+const useStore = <Selection, ContextData, Store>(
+  context: React.Context<ContextData>,
+  storeSelector: (contextData: ContextData) => Store,
+  dataSelector: (store: Store) => Selection,
+) => {
+  const value = React.useContext(context);
+  if (!value) {
+    // type safe
+    throw new Error();
+  }
+  const store = storeSelector(value);
+  return useObserver(() => {
+    return dataSelector(store);
+  });
 };
 
 export default useStore;
