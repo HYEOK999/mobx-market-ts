bb0f88847bdce21fc7b7309f4cd5e8dafad6bdda
HYEOK999 <iasg2004@naver.com>
2020-04-15 23:00:40 +0900

    feat : Updated Component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useObserver } from 'mobx-react';
-import useStore from '../useStore';
+// import useStore from '../useStore';
+import { toJS } from 'mobx';
+import { useRootData } from '../hook';
 
 const Counter: React.FC = ({ children }) => {
-  const { counter } = useStore();
+  // const { counter } = useStore();
+  const { counter, market } = useRootData((store) => store);
 
   const increase = () => {
     counter.increase();
@@ -13,12 +16,14 @@ const Counter: React.FC = ({ children }) => {
     counter.decrease();
   };
 
-  console.log('5151125125', children);
+  console.log('151521', market.selectedItems[0]);
+  console.log('5151125125', toJS(market));
 
   return useObserver(() => (
     <div>
       {console.log('212312')}
       <h1>{counter.number}</h1>
+      <h2>asdasdasd{market.selectedItems[0] && market.selectedItems[0].name}</h2>
       <button onClick={increase}>+1</button>
       <button onClick={decrease}>-1</button>
     </div>
